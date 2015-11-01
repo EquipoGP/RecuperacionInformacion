c05f9cb428eeffb052a9279ad9f5a15a59612780
Patricia Lazaro <plazaro1994@gmail.com>
2015-11-01 19:29:54 +0100

    Trabajo Sistema Tradicional empezado
    Primera version del analizador

---
+++ sistemaTradicional/src/sistemaTradicional/TradicionalAnalyzer.java
@@ -0,0 +1,42 @@
+package sistemaTradicional;
+
+import java.io.Reader;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenFilter;
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.core.LowerCaseFilter;
+import org.apache.lucene.analysis.core.StopFilter;
+import org.apache.lucene.analysis.es.SpanishAnalyzer;
+import org.apache.lucene.analysis.snowball.SnowballFilter;
+import org.apache.lucene.analysis.standard.StandardFilter;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.util.CharArraySet;
+import org.apache.lucene.util.Version;
+import org.tartarus.snowball.ext.SpanishStemmer;
+
+public class TradicionalAnalyzer extends Analyzer {
+
+	public static final String[] CUSTOM_STOP_WORDS = { "interes", "cuy",
+			"relacion", "quier", "gust", "trat", "encontr", "llam", "mund",
+			"sab", "hech", "englob", "period", "document", "pertenezc", "situ" };
+
+	@SuppressWarnings("deprecation")
+	@Override
+	protected TokenStreamComponents createComponents(String field, Reader reader) {
+
+		 CharArraySet stopTable = StopFilter.makeStopSet(CUSTOM_STOP_WORDS, true);
+
+		Tokenizer source = new StandardTokenizer(Version.LUCENE_44, reader);
+
+		TokenFilter filters = new StandardFilter(source);
+		filters = new LowerCaseFilter(filters);
+		filters = new StopFilter(filters, stopTable);
+		filters = new StopFilter(filters, SpanishAnalyzer.getDefaultStopSet());
+		filters = new SnowballFilter(filters, new SpanishStemmer());
+		filters = new StopFilter(filters, stopTable);
+
+		return new TokenStreamComponents(source, filters);
+	}
+
+}
