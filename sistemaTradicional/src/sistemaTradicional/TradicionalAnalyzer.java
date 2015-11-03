7bf55a0d06b82ee0f33c39c906cf10586c8753a0
Patricia Lazaro <plazaro1994@gmail.com>
2015-11-03 16:33:11 +0100

    TradicionalAnalyzer comentado

--- sistemaTradicional/src/sistemaTradicional/TradicionalAnalyzer.java
+++ sistemaTradicional/src/sistemaTradicional/TradicionalAnalyzer.java
@@ -16,16 +16,31 @@ import org.apache.lucene.util.Version;
 import org.tartarus.snowball.ext.SpanishStemmer;
 
 public class TradicionalAnalyzer extends Analyzer {
+	/**
+	 * Analizador especifico del tema Recuperacion de Informacion (2015-2016)
+	 * @version 1.0
+	 */
 
-	public static final String[] CUSTOM_STOP_WORDS = { "interes", "cuy",
-			"relacion", "quier", "gust", "trat", "encontr", "llam", "mund",
-			"sab", "hech", "englob", "period", "document", "pertenezc", "situ" };
-
+	/* Stop Words especificas del tema */
+	public static final String[] CUSTOM_STOP_WORDS = { 
+			"interes", "cuy", "relacion", "quier", "gust", "trat", "encontr", 
+			"llam", "mund", "sab", "hech", "englob", "period", "document", 
+			"pertenezc", "situ" 
+			};
+	
+	/**
+	 * Analizador: crea una cadena de filtros que se compone de:
+	 * - Filtro estandar (signos de puntuacion...)
+	 * - Filtro de lowercase (pasar la entrada a minusculas)
+	 * - Filtro de StopWords genericas del castellano
+	 * - Filtro de stemmer generico del castellano
+	 * - Filtro de StopWords especificas del tema
+	 */
 	@SuppressWarnings("deprecation")
 	@Override
 	protected TokenStreamComponents createComponents(String field, Reader reader) {
 
-		 CharArraySet stopTable = StopFilter.makeStopSet(CUSTOM_STOP_WORDS, true);
+		CharArraySet stopTable = StopFilter.makeStopSet(CUSTOM_STOP_WORDS, true);
 
 		Tokenizer source = new StandardTokenizer(Version.LUCENE_44, reader);
 
