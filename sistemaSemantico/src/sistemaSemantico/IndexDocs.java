d63f67b4387c7f62189a47a65aa59f4bfd2dbe90
Patricia Lazaro <plazaro1994@gmail.com>
2016-02-05 17:08:07 +0100

    ups el warning

--- sistemaSemantico/src/sistemaSemantico/IndexDocs.java
+++ sistemaSemantico/src/sistemaSemantico/IndexDocs.java
@@ -10,7 +10,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
